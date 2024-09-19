5668cb7354738832787931999b32aba7abf96888
Astro-Dust <lucaslucenagdrive@gmail.com>
2024-09-19 19:02:00 -0300

    -

--- data.js
+++ data.js
@@ -2,8 +2,8 @@ getData = () => {
   fetch('http://localhost:8000/get-idols')
     .then((response) => response.json())
     .then((data) => {
-		const idolNames = Object.values(data);
-		console.log(idolNames);
+		// const idolNames = Object.keys(data);
+		console.log(data);
     })
 }
 
